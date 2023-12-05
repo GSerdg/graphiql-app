887a235ec2d39eefc963a72c99a4ea72a05de821
GSerdg <grebelsergey@mail.ru>
2023-12-05 13:49:53 +0300

    feat: add mui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import ErrorBoundary from './components/error-boundary/ErrorBoundary';
 import { Outlet } from 'react-router-dom';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 
 function App() {
