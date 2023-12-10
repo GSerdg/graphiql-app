5e41d97683a126e22adb6c528e5811d72337ff5f
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-12-10 14:28:35 +0300

    fix: update tests

--- src/pages/welcome/Welcome.test.tsx
+++ src/pages/welcome/Welcome.test.tsx
@@ -1,7 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
-import { createMemoryHistory } from 'history';
-import { BrowserRouter, Router } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 import Welcome from './Welcome';
 
@@ -14,32 +12,11 @@ describe('Tests for home component', () => {
     );
 
     const title = screen.getAllByRole('heading');
-    const login = screen.getByRole('link', { name: 'Log in' });
     const divider = screen.getByRole('separator');
 
     expect(title[0]).toHaveTextContent(
       'Debugging your GraphQL server was never this easy!'
     );
-    expect(login).toBeInTheDocument();
     expect(divider).toBeInTheDocument();
-  });
-
-  it('Make sure clicking a link redirects to the appropriate page', async () => {
-    const history = createMemoryHistory();
-
-    render(
-      <Router location={history.location} navigator={history}>
-        <Welcome />
-      </Router>
-    );
-
-    const loginLink = screen.getByRole('link', { name: 'Log in' });
-    const signupLink = screen.getByRole('link', { name: 'Sign up' });
-
-    await userEvent.click(loginLink);
-    expect(history.location.pathname).toBe('/login');
-
-    await userEvent.click(signupLink);
-    expect(history.location.pathname).toBe('/signup');
   });
 });
