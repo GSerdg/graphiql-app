ead2e6cd31dceab12a70706d42a1bba4702ad93c
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-12-20 12:35:19 +0300

    feat: update test for welcome page and footer

--- src/components/footer/Footer.test.tsx
+++ src/components/footer/Footer.test.tsx
@@ -1,15 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
+import { LangContext } from '../../contexts/localization';
+import { SupportedLocales } from '../../localization/language';
 import Footer from './Footer';
+
+const Mocktest = ({ language }: { language: 'ru' | 'en' }) => {
+  const [lang, setLang] = useState<SupportedLocales>(language);
+
+  return (
+    <BrowserRouter>
+      <LangContext.Provider value={{ lang, setLang }}>
+        <Footer />
+      </LangContext.Provider>
+    </BrowserRouter>
+  );
+};
 
 describe('Tests for footer component', () => {
   it('Make sure the component is rendering', () => {
-    render(
-      <BrowserRouter>
-        <Footer />
-      </BrowserRouter>
-    );
+    render(<Mocktest language="en" />);
 
     const footer = screen.getByRole('contentinfo');
     const rssLogo = screen.getByRole('link', { name: 'rss_logo' });
--- src/pages/welcome/Welcome.test.tsx
+++ src/pages/welcome/Welcome.test.tsx
@@ -1,20 +1,47 @@
 import { render, screen } from '@testing-library/react';
+import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
+import { LangContext } from '../../contexts/localization';
+import { SupportedLocales } from '../../localization/language';
 import Welcome from './Welcome';
 
-describe('Tests for home component', () => {
-  it('Make sure the component is rendering', () => {
-    render(
-      <BrowserRouter>
+const Mocktest = ({ language }: { language: 'ru' | 'en' }) => {
+  const [lang, setLang] = useState<SupportedLocales>(language);
+
+  return (
+    <BrowserRouter>
+      <LangContext.Provider value={{ lang, setLang }}>
         <Welcome />
-      </BrowserRouter>
-    );
+      </LangContext.Provider>
+    </BrowserRouter>
+  );
+};
+
+describe('Tests for home component', () => {
+  it('Make sure the component is rendering in English', () => {
+    render(<Mocktest language="en" />);
 
     const title = screen.getAllByRole('heading');
     const divider = screen.getByRole('separator');
+    const chip = screen.getByText('OUR TEAM');
 
     expect(title[0]).toHaveTextContent('Debugging your GraphQL server was never this easy!');
     expect(divider).toBeInTheDocument();
+    expect(chip).toBeInTheDocument();
+  });
+
+  it('Make sure the component is rendering in Russian', () => {
+    render(<Mocktest language="ru" />);
+
+    const title = screen.getAllByRole('heading');
+    const chip = screen.getByText('НАША КОМАНДА');
+    const role = screen.getAllByText('Разработчик');
+
+    expect(title[0]).toHaveTextContent('Отладка вашего сервера GraphQL никогда не была такой простой!');
+    expect(chip).toBeInTheDocument();
+    expect(role[0]).toBeInTheDocument();
+    expect(role[1]).toBeInTheDocument();
+    expect(role[2]).toBeInTheDocument();
   });
 });
