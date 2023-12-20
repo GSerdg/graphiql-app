faed8570d6a4af2505eaf371f65520e4e7b778a0
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-12-20 10:42:21 +0300

    feat: update header tests

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -4,32 +4,43 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { BrowserRouter } from 'react-router-dom';
 import { MockedFunction, describe, expect, it, vi } from 'vitest';
 import Header from './Header';
+import { SupportedLocales } from '../../localization/language';
+import { useState } from 'react';
+import { LangContext } from '../../contexts/localization';
 
 vi.mock('react-firebase-hooks/auth');
 
+const Mocktest = ({ language }: { language: 'ru' | 'en' }) => {
+  const [lang, setLang] = useState<SupportedLocales>(language);
+
+  return (
+    <BrowserRouter>
+      <LangContext.Provider value={{ lang, setLang }}>
+        <Header />
+      </LangContext.Provider>
+    </BrowserRouter>
+  );
+};
+
 describe('Tests for header component', () => {
-  it('Make sure the component is rendering', () => {
+  it('Make sure the component is rendering in English', async () => {
     (useAuthState as MockedFunction<typeof useAuthState>).mockReturnValue([undefined, false, undefined]);
 
-    render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    );
+    render(<Mocktest language="en" />);
 
     const header = screen.getByRole('banner');
     const buttonGroup = screen.getByRole('group');
-    const mainLink = screen.getByRole('link', { name: 'Home' });
+    const homeLink = screen.getByRole('link', { name: 'Home' });
 
     expect(header).toBeInTheDocument();
     expect(buttonGroup).toBeInTheDocument();
-    expect(mainLink).toBeInTheDocument();
+    expect(homeLink).toBeInTheDocument();
   });
 
   it('Make sure clicking a link redirects to the appropriate page', async () => {
     (useAuthState as MockedFunction<typeof useAuthState>).mockReturnValue([undefined, false, undefined]);
 
-    render(<Header />, { wrapper: BrowserRouter });
+    render(<Mocktest language="en" />);
 
     const mainLink = screen.getByRole('link', { name: 'Home' });
     const loginLink = screen.getByRole('link', { name: 'Log in' });
@@ -45,18 +56,17 @@ describe('Tests for header component', () => {
     expect(window.location.pathname).toBe('/');
   });
 
-  it('Check that langSwitcher works', async () => {
+  it('Make sure the component is rendering in Russian', async () => {
     (useAuthState as MockedFunction<typeof useAuthState>).mockReturnValue([undefined, false, undefined]);
 
-    render(
-      <BrowserRouter>
-        <Header />
-      </BrowserRouter>
-    );
+    render(<Mocktest language="ru" />);
 
-    const langSwitcher = screen.getByTestId('langSwitcher');
-    await userEvent.click(langSwitcher);
+    const homeLink = screen.getByRole('link', { name: 'Домой' });
+    const loginLink = screen.getByRole('link', { name: 'Вход' });
+    const signupLink = screen.getByRole('link', { name: 'Регистрация' });
 
-    //TODO Test for russian language
+    expect(homeLink).toBeInTheDocument();
+    expect(loginLink).toBeInTheDocument();
+    expect(signupLink).toBeInTheDocument();
   });
 });
