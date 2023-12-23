07770cc5036e504319d4b46e05bef8e7deae2927
user42022 <andrekizilov@yandex.ru>
2023-12-23 16:38:43 +0300

    feat: add layout to show runtime error

---
+++ src/components/error-boundary/ErrorAlert.tsx
@@ -0,0 +1,54 @@
+import { Collapse, Alert, IconButton, AlertTitle, Fade, Tooltip } from '@mui/material';
+import { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
+import ExpandCircleDownIcon from '@mui/icons-material/ExpandCircleDown';
+
+interface ErrorAlertProps {
+  error: Error;
+}
+
+export default function ErrorAlert(props: ErrorAlertProps) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [isShowDetailed, setIsShowDetailed] = useState(false);
+
+  return (
+    <Fade in={isOpen}>
+      <Alert
+        severity={'error'}
+        action={
+          <>
+            <Tooltip title={'Show error stack'}>
+              <IconButton
+                sx={{ transition: 'transform .5s', transform: `rotate(${isShowDetailed ? 180 : 0}deg)` }}
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setIsShowDetailed(!isShowDetailed);
+                }}
+              >
+                <ExpandCircleDownIcon fontSize="inherit" />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title={'Close error alert'}>
+              <IconButton
+                aria-label="close"
+                color="inherit"
+                size="small"
+                onClick={() => {
+                  setIsOpen(false);
+                }}
+              >
+                <CloseIcon fontSize="inherit" />
+              </IconButton>
+            </Tooltip>
+          </>
+        }
+        sx={{ width: '75%', margin: '1rem auto 0', minWidth: '250px' }}
+      >
+        <AlertTitle>{props.error.message}</AlertTitle>
+        <Collapse in={isShowDetailed}>{props.error.stack}</Collapse>
+      </Alert>
+    </Fade>
+  );
+}
--- src/components/error-boundary/ErrorBoundary.tsx
+++ src/components/error-boundary/ErrorBoundary.tsx
@@ -1,28 +1,52 @@
 import { Component } from 'react';
 import './ErrorBoundary.scss';
+import { Box, Container, CssBaseline, Typography } from '@mui/material';
+import ErrorAlert from './ErrorAlert';
+import '../../index.scss';
 
 interface ErrorProps {
   children: JSX.Element;
 }
 interface ErrorState {
-  hasError: boolean;
+  error: null | Error;
 }
 
 class ErrorBoundary extends Component<ErrorProps, ErrorState> {
   constructor(props: ErrorProps) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { error: null };
   }
 
   static getDerivedStateFromError(error: Error) {
-    console.error(error);
-    return { hasError: true };
+    return { error };
   }
   static componentDidCatch(error: Error) {
     console.error(error);
   }
   render() {
-    return this.state.hasError ? <h1 className="error-header">Ohh Error!!!</h1> : this.props.children;
+    return this.state.error ? (
+      <CssBaseline>
+        <Box className="error-wrapper">
+          <Container>
+            <Box className="error-header">
+              <Box sx={{ color: 'white' }}>
+                <Typography component={'h1'} sx={{ fontSize: '2rem' }}>
+                  Something went wrong
+                </Typography>
+                <Typography>
+                  An unexpected error occurred while the application was running. Try to reload page
+                </Typography>
+              </Box>
+
+              <img className="error-image" src="svg/error-905.svg" alt="Error image" />
+            </Box>
+            <ErrorAlert error={this.state.error}></ErrorAlert>
+          </Container>
+        </Box>
+      </CssBaseline>
+    ) : (
+      this.props.children
+    );
   }
 }
 
