5d87b3a03dc92ec42509cee396913b0812976cee
user42022 <andrekizilov@yandex.ru>
2023-12-23 17:50:22 +0300

    feat: modify test for localization

--- src/components/error-boundary/ErrorBoundary.test.tsx
+++ src/components/error-boundary/ErrorBoundary.test.tsx
@@ -2,24 +2,45 @@ import { screen } from '@testing-library/react';
 import { describe, expect, it, vi } from 'vitest';
 import ErrorBoundary from './ErrorBoundary';
 import { renderWithProviders } from '../../test/testUtils';
+import Provider from '../../contexts/Provider';
 
 const errorName = 'generated error';
 const error = new Error(errorName);
-const WithError = () => {
+const ComponentWithError = () => {
   throw error;
+};
+const Mocktest = () => {
+  return (
+    <ErrorBoundary>
+      <ComponentWithError />
+    </ErrorBoundary>
+  );
 };
 
 describe('Tests for ErrorBoundary', () => {
   vi.spyOn(console, 'error').mockImplementation(() => null);
 
   it('Make sure the component is rendering', () => {
-    renderWithProviders(<WithError />, { wrapper: ErrorBoundary });
+    renderWithProviders(<Mocktest />, { wrapper: Provider });
     const title = screen.getByRole('heading');
     const description = screen.getByText(
       'An unexpected error occurred while the application was running. Try to reload page'
     );
 
     expect(title).toHaveTextContent('Something went wrong');
+    expect(description).toBeInTheDocument();
+  });
+
+  it('Make sure the component is rendering', () => {
+    localStorage.setItem('lang', 'ru');
+
+    renderWithProviders(<Mocktest />, { wrapper: Provider });
+    const title = screen.getByRole('heading');
+    const description = screen.getByText(
+      'Во время работы приложения произошла непредвиденная ошибка. Попробуйте перезагрузить страницу'
+    );
+
+    expect(title).toHaveTextContent('Что-то пошло не так');
     expect(description).toBeInTheDocument();
   });
 });
