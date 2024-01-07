164d49a9fb7c3a0cde5616e0429d35a3f83aaf35
GSerdg <grebelsergey@mail.ru>
2024-01-07 17:46:03 +0300

    refactor: delete comments

--- src/test/handler.ts
+++ src/test/handler.ts
@@ -3,10 +3,6 @@ import COUNTRIES_SCHEMA, { COUNTRIES_API } from './schemes/countries';
 import RICK_MORTY_SCHEMA, { RICK_MORTY_API } from './schemes/rick-and-morty';
 
 export const handlers = [
-  // http.get('https://swapi.dev/api/people/*', async () => {
-  //   await delay(150);
-  //   return HttpResponse.json({ value: 'data' });
-  // }),
   http.post(RICK_MORTY_API, async () => {
     await delay(150);
     return HttpResponse.json({ data: { __schema: RICK_MORTY_SCHEMA } });
