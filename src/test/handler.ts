e8c11c5d075eae094decf62ea0a066bb82020de1
user42022 <andrekizilov@yandex.ru>
2024-01-07 11:16:06 +0300

    feat: add tests for documentation section, update handlers

--- src/test/handler.ts
+++ src/test/handler.ts
@@ -1,8 +1,18 @@
 import { HttpResponse, delay, http } from 'msw';
+import RICK_MORTY_SCHEMA, { RICK_MORTY_API } from './schemes/rick-and-morty';
+import COUNTRIES_SCHEMA, { COUNTRIES_API } from './schemes/countries';
 
 export const handlers = [
   http.get('https://swapi.dev/api/people/*', async () => {
     await delay(150);
     return HttpResponse.json({ value: 'data' });
+  }),
+  http.get(RICK_MORTY_API, async () => {
+    await delay(150);
+    return HttpResponse.json({ data: { __schema: RICK_MORTY_SCHEMA } });
+  }),
+  http.get(COUNTRIES_API, async () => {
+    await delay(150);
+    return HttpResponse.json({ data: { __schema: COUNTRIES_SCHEMA } });
   }),
 ];
